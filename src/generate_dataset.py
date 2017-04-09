b10e61b3ece01dd6717d9b889040f6dce5446acd
Shimo, Hayato <shimo.8810@gmail.com>
2017-04-09 19:47:05 +0900

    modified

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-import sys
-import nupy as np
-import cv2
-import glob
-=======
 import sys
 import numpy as np
 import cv2
 import glob
->>>>>>> refs/remotes/origin/master
 import chainer
 from chainer import datasets
 
 class ImageDataset(chainer.dataset.DatasetMixin):
     """docstring forImageDataset."""
-<<<<<<< HEAD
     def __init__(self):
         i = 1
         data_paths = glob.glob('../images/demo_train_dataset/*')
@@ -33,23 +25,3 @@ if __name__ == '__main__':
     image_dataset = ImageDataset()
     print(len(image_dataset))
     image_dataset.get_example(2)
-=======
-    def __init__(self):
-        i = 1
-        data_paths = glob.glob('../images/demo_train_dataset/*')
-        pairs = []
-        for path in data_paths:
-            pairs.append(np.load(path))
-        self.pairs = pairs
-
-    def __len__(self):
-        return len(self.pairs)
-
-    def get_example(self, i):
-        image_input, image_label = self.pairs[i]
-        return image_input, image_label
-if __name__ == '__main__':
-    image_dataset = ImageDataset()
-    print(len(image_dataset))
-    image_dataset.get_example(2)
->>>>>>> refs/remotes/origin/master
