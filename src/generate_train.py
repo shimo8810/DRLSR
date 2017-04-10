7840062c671a064c506c43bbbfec69833cd78041
Hayato Shimodaira <shimodaira@cmu.iit.tsukuba.ac.jp>
2017-04-10 19:04:03 +0900

    parallelized

--- src/generate_train.py
+++ src/generate_train.py
@@ -5,6 +5,8 @@ import numpy as np
 import cv2
 import os
 import sys
+from joblib import Parallel, delayed
+
 #parameter
 SIZE_INPUT = 41
 SIZE_LABEL = 41
@@ -25,15 +27,9 @@ count = 0
 length = len(image_paths)
 im_no = 0
 #データ生成
-for i in image_paths:
-    count += 1
-    #データ少なく
-    if count > 1:
-        break
-
-    sys.stdout.write("\r images :{}/{}, {}％".format(count, length, (count*100)//length))
-    sys.stdout.flush()
-
+#並列化 関数
+def gen_train(i):
+    c = 0
     #chainer ではfloat32を読み込むがmatlab版では倍精度に変換している
     image = cv2.imread(image_path+i).astype(np.float32)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
@@ -43,36 +39,22 @@ for i in image_paths:
     #ラベル用画像
     image_label = image[0:size[0], 0:size[1]]
     height, width = image_label.shape
-
     #中間縮小画像
     buf = cv2.resize(image, (width//SCALE, height//SCALE), \
                      interpolation=cv2.INTER_CUBIC)
     #入力用画像
     image_input = cv2.resize(buf, (width, height), \
                              interpolation=cv2.INTER_CUBIC)
-
     #各画像を入力サイズに切り分けていく
     for x in range(0, height - SIZE_INPUT + 1, STRIDE):
         for y in range(0, width - SIZE_INPUT + 1, STRIDE):
             subim_input = image_input[np.newaxis, np.newaxis, x:x+SIZE_INPUT, y:y+SIZE_INPUT]
             subim_label = image_label[np.newaxis, np.newaxis, x:x+SIZE_INPUT, y:y+SIZE_INPUT]
 
-
             #雑魚セクション
             _data = np.concatenate([subim_input, subim_label], axis=0)
-            np.save('../images/demo_train_dataset/' + str(im_no) + '.npy', _data)
-            im_no += 1
-
-            #train, label配列にどんどんデータを追加
-            if train is None and label is None:
-                train =  subim_input
-                label = subim_label
-            else:
-                train = np.concatenate([train, subim_input], axis=0)
-                label = np.concatenate([label, subim_label], axis=0)
-
-    #データのシャッフル（不要？）
-np.save('../images/train_data.npy', train)
-np.save('../images/label_data.npy', label)
-print("\n", train.shape)
-print("\nsaved data to npy")
+            np.save('../images/demo_train_dataset/' + str(i.split('.')[0]) + '_' + str(c) + '.npy', _data)
+            c += 1
+#並列処理
+Parallel(n_jobs=-1, verbose=5)([delayed(gen_train)(i) for i in image_paths])
+print("saved data to npy")
